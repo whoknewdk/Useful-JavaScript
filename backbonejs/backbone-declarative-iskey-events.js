37a382e691ca5bafe7f52d31c4e8e1c0292f0768
Jacob T. Nielsen <jacob@whoknew.dk>
2014-05-10 14:04:05 +0200

    Added backbone declarative iskey events

---
+++ backbonejs/backbone-declarative-iskey-events.js
@@ -0,0 +1,45 @@
+;(function () {
+	var slice = Array.prototype.slice;
+
+	function ifKeyIs(keyCode, method) {
+		return function (e) {
+			if (e.which === keyCode)
+				method.apply(this, slice.apply(arguments));
+		};
+	}
+
+	var delegateEvents = Backbone.View.prototype.delegateEvents,
+		eventKeyNameSplitter = /^(\S+):iskey\((13)\)\s(.*)$/;
+
+	Backbone.View.prototype.delegateEvents = function (events) {
+		if (!(events || (events = _.result(this, 'events'))))
+			return this;
+
+		for (var key in events) {
+			var method = events[key],
+				match = key.match(eventKeyNameSplitter);
+			
+			// Does key follow :iskey(...) schema?
+			if (!match)
+				continue;
+			
+			// Delete original key
+			delete events[key];
+
+			// Map method-key to function if not already a function
+			if (!_.isFunction(method))
+				method = this[method];
+
+			if (!method)
+				continue;
+
+			// Add sanitized key
+			events[match[1] + ' ' + match[3]] = ifKeyIs(parseInt(match[2], 10), method);
+		}
+
+		// Run everything as normal
+		delegateEvents.apply(this, slice.apply(arguments));
+
+		return this;
+	};
+}());
